af32d6bbfc2de27d904e9319c43739b97d6f9619
knoxb1atwit <knoxb1@wit.edu>
2026-03-20 13:06:29 -0400

    Blank GUI

--- SchedulingSoftware/src/project/Main.java
+++ SchedulingSoftware/src/project/Main.java
@@ -1,11 +1,32 @@
 package project;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class Main {
+public Main() {
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	panel.setBorder(BorderFactory.createEmptyBorder(500, 500, 250, 500));
+	panel.setLayout(new GridLayout(0, 1));
+	
+	frame.add(panel, BorderLayout.CENTER);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setTitle("Schudle");
+	frame.pack();
+	frame.setVisible(true);
+}
 
 	public static void main(String[] args) {
+		
+		new Main();
 		
 		// Create a schedule
 		Schedule schedule = new Schedule(LocalTime.of(9, 0), LocalTime.of(17, 0));
